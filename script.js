99b0ffbdc3e2de4e2b6b5db9af75f0d9678fc956
oseji <jiade1233@gmail.com>
2023-09-25 11:43:14 +0100

    mobile design

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const navbar = document.querySelector(".navbar");
+const landingPage = document.querySelector(".landingPage");
+const contactPage = document.querySelector(".contactPage");
+const registarationPage = document.querySelector(".registarationPage");
+
+navbar.addEventListener("click", (e) => {
+  const clicked = e.target;
+  console.log(clicked);
+
+  if (clicked.id == "contactLink") {
+    e.preventDefault();
+    contactPage.classList.remove("extraPageHidden");
+
+    contactPage.classList.add("extraPageVisible");
+    registarationPage.classList.remove("extraPageVisible");
+    registarationPage.classList.add("extraPageHidden");
+    landingPage.classList.add("extraPageHidden");
+  }
+
+  if (clicked.id == "registrationLink") {
+    e.preventDefault();
+    registarationPage.classList.remove("extraPageHidden");
+
+    registarationPage.classList.add("extraPageVisible");
+    contactPage.classList.remove("extraPageVisible");
+    contactPage.classList.add("extraPageHidden");
+    landingPage.classList.add("extraPageHidden");
+  }
+
+  if (clicked.id == "overview") {
+    e.preventDefault();
+    landingPage.classList.remove("extraPageHidden");
+
+    registarationPage.classList.add("extraPageHidden");
+    registarationPage.classList.remove("extraPageVisible");
+    contactPage.classList.add("extraPageHidden");
+    contactPage.classList.remove("extraPageVisible");
+  }
+});
